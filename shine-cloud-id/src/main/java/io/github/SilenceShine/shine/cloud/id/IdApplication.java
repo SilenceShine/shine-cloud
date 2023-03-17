44c9681bacc133f7ade94eaf5b79e6ff456042c4
SilenceRainly <1292798418@qq.com>
2023-03-17 17:36:54 +0800

    init

---
+++ shine-cloud-id/src/main/java/io/github/SilenceShine/shine/cloud/id/IdApplication.java
@@ -0,0 +1,17 @@
+package io.github.SilenceShine.shine.cloud.id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SilenceShine
+ * @since 1.0
+ */
+@SpringBootApplication
+public class IdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdApplication.class, args);
+    }
+
+}
